1506088bd102fb39740680a7b26aa76a6d01ebd8
thallysonpontes <thallysonsalazar@hotmail.com>
2020-04-30 21:54:05 -0300

    Criando Api de vendas

---
+++ src/test/java/com/example/thallysonpontes/vendas/VendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thallysonpontes.vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
